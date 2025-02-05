7cdbee7694de43a282d399baa9dfa74fd64b51c8
wokakse <ryzgaaa@gmail.com>
2025-02-06 03:02:42 +0700

    Update navigation.ts

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -7,8 +7,8 @@ export interface Framework {
 
 export const frameworks: Framework[] = [
     { name: 'astro', displayName: 'Astro', path: '/', color: '#FF5D01' },
-    { name: 'react', displayName: 'React', path: '/react', color: '#61DAFB' },
+    <!-- { name: 'react', displayName: 'React', path: '/react', color: '#61DAFB' },
     { name: 'vuejs', displayName: 'Vue', path: '/vue', color: '#4FC08D' },
     { name: 'svelte', displayName: 'Svelte', path: '/svelte', color: '#FF3E00' },
-    { name: 'solidjs', displayName: 'Solid', path: '/solid', color: '#2C4F7C' },
+    { name: 'solidjs', displayName: 'Solid', path: '/solid', color: '#2C4F7C' }, -->
 ];
